8bce289293bbe2dd651b520438dcc6da8a72b935
aniket662 <deya7153@gmail.com>
2024-04-16 12:01:48 +0530

    updated spawn player script

--- Assets/SpawnPlayers.cs
+++ Assets/SpawnPlayers.cs
@@ -12,8 +12,24 @@ public class SpawnPlayers : MonoBehaviour
     public float maxY;
 
     void Start(){
-        Vector2 randomPosition = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        /*Vector2 randomPosition = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);*/
+        if (PhotonNetwork.IsConnected && PhotonNetwork.IsMasterClient)
+        {
+            SpawnPlayer();
+        }
+    }
+    void SpawnPlayer()
+    {
+        // Generate a random position within the specified range
+        Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        
+        // Instantiate the player prefab only on the local client
+        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        
+        // Ensure that the local client owns the PhotonView of the player object
+        PhotonView pv = player.GetComponent<PhotonView>();
+        pv.TransferOwnership(PhotonNetwork.LocalPlayer);
     }
 }
 
